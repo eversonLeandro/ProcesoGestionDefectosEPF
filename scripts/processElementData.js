b83c7c198eb772a92cd8087076023a9f80c6b785
everson <eversonrestrepo@unicauca.edu.co>
2026-09-21 18:12:58 -0500

    Initial commit

---
+++ scripts/processElementData.js
@@ -0,0 +1,75 @@
+//------------------------------------------------------------------------------
+// Copyright (c) 2005, 2006 IBM Corporation and others.
+// All rights reserved. This program and the accompanying materials
+// are made available under the terms of the Eclipse Public License v1.0
+// which accompanies this distribution, and is available at
+// http://www.eclipse.org/legal/epl-v10.html
+// 
+// Contributors:
+// IBM Corporation - initial implementation
+//------------------------------------------------------------------------------
+
+// This file will include dynamically generated process layout related data from 
+// the publishing service 
+contentPage.processPage.imageFiles["_1KCU0bVUEfGBKOhJTM5JFQActivity"]="./../../GestionDefectosPlugin/capabilitypatterns/resources/registro_triage_D884657_d884657_Activity.jpeg"
+contentPage.processPage.imageFiles["_1KCU0bVUEfGBKOhJTM5JFQActivityDetail"]="./../../GestionDefectosPlugin/capabilitypatterns/resources/registro_triage_D884657_d884657_ActivityDetail.jpeg"
+contentPage.processPage.imageFiles["_lawGkbVWEfG8usiShKwhWwActivity"]="./../../GestionDefectosPlugin/capabilitypatterns/resources/resolucion_753D290A_753d290a_Activity.jpeg"
+contentPage.processPage.imageFiles["_lawGkbVWEfG8usiShKwhWwActivityDetail"]="./../../GestionDefectosPlugin/capabilitypatterns/resources/resolucion_753D290A_753d290a_ActivityDetail.jpeg"
+contentPage.processPage.imageFiles["_1BjcUbVWEfG8usiShKwhWwActivity"]="./../../GestionDefectosPlugin/capabilitypatterns/resources/Verificacion_cierre_C4C6CB65_c4c6cb65_Activity.jpeg"
+contentPage.processPage.imageFiles["_1BjcUbVWEfG8usiShKwhWwActivityDetail"]="./../../GestionDefectosPlugin/capabilitypatterns/resources/Verificacion_cierre_C4C6CB65_c4c6cb65_ActivityDetail.jpeg"
+contentPage.processPage.imageFiles["_VVHqUbVXEfG8usiShKwhWw,_wf7Ow7VXEfG8usiShKwhWwActivity"]="./../../GestionDefectosPlugin/deliveryprocesses/resources/registro_triage_5002936_6e2e952f_Activity.jpeg"
+contentPage.processPage.imageFiles["_VVHqUbVXEfG8usiShKwhWw,_wf7Ow7VXEfG8usiShKwhWwActivityDetail"]="./../../GestionDefectosPlugin/deliveryprocesses/resources/registro_triage_5002936_6e2e952f_ActivityDetail.jpeg"
+contentPage.processPage.imageFiles["_VVHqUbVXEfG8usiShKwhWwActivity"]="./../../GestionDefectosPlugin/deliveryprocesses/resources/gestion_defectos_7BC3CA5_7bc3ca5_Activity.jpeg"
+contentPage.processPage.imageFiles["_VVHqUbVXEfG8usiShKwhWw,_8Th_trVXEfG8usiShKwhWwActivityDetail"]="./../../GestionDefectosPlugin/deliveryprocesses/resources/Verificacion_cierre_8166A120_ea950d19_ActivityDetail.jpeg"
+contentPage.processPage.imageFiles["_VVHqUbVXEfG8usiShKwhWw,_4ZP7cbVXEfG8usiShKwhWwActivityDetail"]="./../../GestionDefectosPlugin/deliveryprocesses/resources/resolucion_C0CC347B_29faa074_ActivityDetail.jpeg"
+contentPage.processPage.elementUrls["_4ZPUYbVXEfG8usiShKwhWw"] = ["Análisis de causa raíz","GestionDefectosPlugin/deliveryprocesses/analisis_de_causa_raiz_2D10D3E3.html"];
+contentPage.processPage.elementUrls["_8Th_t7VXEfG8usiShKwhWw"] = ["Reportante","GestionDefectosPlugin/deliveryprocesses/reportante_EA4076E5.html"];
+contentPage.processPage.elementUrls["_Fezj0bVVEfGBKOhJTM5JFQ"] = ["Líder técnico","GestionDefectosPlugin/capabilitypatterns/lider_tecnico_5AA401FB.html"];
+contentPage.processPage.elementUrls["_4ZPUYrVXEfG8usiShKwhWw"] = ["Corrección","GestionDefectosPlugin/deliveryprocesses/correccin_37AEB3F3.html"];
+contentPage.processPage.elementUrls["_565J17VWEfG8usiShKwhWw"] = ["Corrección","GestionDefectosPlugin/capabilitypatterns/correccin_C812FCA.html"];
+contentPage.processPage.elementUrls["_D6uh8LVVEfGBKOhJTM5JFQ"] = ["Reporte de defecto","GestionDefectosPlugin/capabilitypatterns/reporte_de_defecto_11FB8393.html"];
+contentPage.processPage.elementUrls["_8Th_ubVXEfG8usiShKwhWw"] = ["Tester","GestionDefectosPlugin/deliveryprocesses/tester_3B5AA32F.html"];
+contentPage.processPage.elementUrls["_AgN4A7VXEfG8usiShKwhWw"] = ["Defecto cerrado","GestionDefectosPlugin/capabilitypatterns/defecto_cerrado_593001D1.html"];
+contentPage.processPage.elementUrls["_wf710rVXEfG8usiShKwhWw"] = ["Priorizar y asignar defecto","GestionDefectosPlugin/deliveryprocesses/priorizar_y_asignar_defecto_FDB9283A.html"];
+contentPage.processPage.elementUrls["_wf7Ow7VXEfG8usiShKwhWw"] = ["Registro y triage","GestionDefectosPlugin/deliveryprocesses/registro_triage_5002936.html"];
+contentPage.processPage.elementUrls["_565J1bVWEfG8usiShKwhWw"] = ["Reporte de defecto","GestionDefectosPlugin/capabilitypatterns/reporte_de_defecto_990979F5.html"];
+contentPage.processPage.elementUrls["_wf710bVXEfG8usiShKwhWw"] = ["Líder técnico","GestionDefectosPlugin/deliveryprocesses/lider_tecnico_F31B482A.html"];
+contentPage.processPage.elementUrls["_wf7OwLVXEfG8usiShKwhWw"] = ["Reporte de defecto","GestionDefectosPlugin/deliveryprocesses/reporte_de_defecto_A2EF5F4B.html"];
+contentPage.processPage.elementUrls["_4ZP7crVXEfG8usiShKwhWw"] = ["Corregir defecto","GestionDefectosPlugin/deliveryprocesses/corregir_defecto_CB6A148B.html"];
+contentPage.processPage.elementUrls["_wDT2ILVVEfGBKOhJTM5JFQ"] = ["Defecto asignado","GestionDefectosPlugin/capabilitypatterns/Defecto asignado_5EDFE168.html"];
+contentPage.processPage.elementUrls["_wf7107VXEfG8usiShKwhWw"] = ["Reportante","GestionDefectosPlugin/deliveryprocesses/reportante_6692FDFF.html"];
+contentPage.processPage.elementUrls["_8Th_trVXEfG8usiShKwhWw"] = ["Verificación y cierre","GestionDefectosPlugin/deliveryprocesses/Verificacion_cierre_8166A120.html"];
+contentPage.processPage.elementUrls["_q79FULVWEfG8usiShKwhWw"] = ["Analizar causa raíz","GestionDefectosPlugin/capabilitypatterns/analizar_causa_raz_4AC2B778.html"];
+contentPage.processPage.elementUrls["_D6t64LVVEfGBKOhJTM5JFQ"] = ["Reportar defecto","GestionDefectosPlugin/capabilitypatterns/reportar_defecto_ECCB0D06.html"];
+contentPage.processPage.elementUrls["_565J2bVWEfG8usiShKwhWw"] = ["Evidencia de prueba","GestionDefectosPlugin/capabilitypatterns/evidencia_de_prueba_5D9B5C14.html"];
+contentPage.processPage.elementUrls["_8Th_u7VXEfG8usiShKwhWw"] = ["Evidencia de prueba","GestionDefectosPlugin/deliveryprocesses/evidencia_de_prueba_AED25904.html"];
+contentPage.processPage.elementUrls["_8Th_sLVXEfG8usiShKwhWw"] = ["Verificar corrección","GestionDefectosPlugin/deliveryprocesses/verificar_correccin_C39DCADB.html"];
+contentPage.processPage.elementUrls["_q8AvsrVWEfG8usiShKwhWw"] = ["Corrección","GestionDefectosPlugin/capabilitypatterns/correccin_26AC27E9.html"];
+contentPage.processPage.elementUrls["_rbIUoLVWEfG8usiShKwhWw"] = ["Corregir defecto","GestionDefectosPlugin/capabilitypatterns/corregir_defecto_6CE3C627.html"];
+contentPage.processPage.elementUrls["_1BjcUbVWEfG8usiShKwhWw"] = ["Verificación y cierre","GestionDefectosPlugin/capabilitypatterns/Verificacion_cierre_C4C6CB65.html"];
+contentPage.processPage.elementUrls["_q79sYLVWEfG8usiShKwhWw"] = ["Desarrollador","GestionDefectosPlugin/capabilitypatterns/desarrollador_83FD6EE1.html"];
+contentPage.processPage.elementUrls["_AgN4AbVXEfG8usiShKwhWw"] = ["Líder técnico","GestionDefectosPlugin/capabilitypatterns/lider_tecnico_E5B84BFC.html"];
+contentPage.processPage.elementUrls["_565J07VWEfG8usiShKwhWw"] = ["Reportante","GestionDefectosPlugin/capabilitypatterns/reportante_47EF4DAB.html"];
+contentPage.processPage.elementUrls["_q7-TcLVWEfG8usiShKwhWw"] = ["Reporte de defecto","GestionDefectosPlugin/capabilitypatterns/reporte_de_defecto_6E786144.html"];
+contentPage.processPage.elementUrls["_8Th_tLVXEfG8usiShKwhWw"] = ["Líder técnico","GestionDefectosPlugin/deliveryprocesses/lider_tecnico_882FACFA.html"];
+contentPage.processPage.elementUrls["_8Th_tbVXEfG8usiShKwhWw"] = ["Reporte de defecto","GestionDefectosPlugin/deliveryprocesses/reporte_de_defecto_76C8C110.html"];
+contentPage.processPage.elementUrls["_4ZP7cbVXEfG8usiShKwhWw"] = ["resolucion","GestionDefectosPlugin/deliveryprocesses/resolucion_C0CC347B.html"];
+contentPage.processPage.elementUrls["_4ZPUY7VXEfG8usiShKwhWw"] = ["Analizar causa raíz","GestionDefectosPlugin/deliveryprocesses/analizar_causa_raz_A08889B8.html"];
+contentPage.processPage.elementUrls["_565J0LVWEfG8usiShKwhWw"] = ["Verificar corrección","GestionDefectosPlugin/capabilitypatterns/verificar_correccin_E5DE83C0.html"];
+contentPage.processPage.elementUrls["_565J0bVWEfG8usiShKwhWw"] = ["Tester","GestionDefectosPlugin/capabilitypatterns/tester_D47797D6.html"];
+contentPage.processPage.elementUrls["_8Th_s7VXEfG8usiShKwhWw"] = ["Defecto verificado","GestionDefectosPlugin/deliveryprocesses/Defecto verificado_25AE94C6.html"];
+contentPage.processPage.elementUrls["_1KCU0bVUEfGBKOhJTM5JFQ"] = ["Registro y triage","GestionDefectosPlugin/capabilitypatterns/registro_triage_D884657.html"];
+contentPage.processPage.elementUrls["_4ZPUZbVXEfG8usiShKwhWw"] = ["Desarrollador","GestionDefectosPlugin/deliveryprocesses/desarrollador_F1A2B602.html"];
+contentPage.processPage.elementUrls["_8Th_srVXEfG8usiShKwhWw"] = ["Defecto cerrado","GestionDefectosPlugin/deliveryprocesses/defecto_cerrado_BCD4BF01.html"];
+contentPage.processPage.elementUrls["_wf711LVXEfG8usiShKwhWw"] = ["Defecto asignado","GestionDefectosPlugin/deliveryprocesses/Defecto asignado_C9141633.html"];
+contentPage.processPage.elementUrls["_4ZPUZLVXEfG8usiShKwhWw"] = ["Reporte de defecto","GestionDefectosPlugin/deliveryprocesses/reporte_de_defecto_309A1EC.html"];
+contentPage.processPage.elementUrls["_8Th_uLVXEfG8usiShKwhWw"] = ["Corrección","GestionDefectosPlugin/deliveryprocesses/correccin_4CC18F19.html"];
+contentPage.processPage.elementUrls["_Fezj0LVVEfGBKOhJTM5JFQ"] = ["Priorizar y asignar defecto","GestionDefectosPlugin/capabilitypatterns/priorizar_y_asignar_defecto_6C0AEDE5.html"];
+contentPage.processPage.elementUrls["_AgN4ALVXEfG8usiShKwhWw"] = ["Cerrar defecto","GestionDefectosPlugin/capabilitypatterns/cerrar_defecto_F71F37E6.html"];
+contentPage.processPage.elementUrls["_D6t64bVVEfGBKOhJTM5JFQ"] = ["Reportante","GestionDefectosPlugin/capabilitypatterns/reportante_DB64211C.html"];
+contentPage.processPage.elementUrls["_8rDkULVWEfG8usiShKwhWw"] = ["Defecto verificado","GestionDefectosPlugin/capabilitypatterns/Defecto verificado_EBD115C6.html"];
+contentPage.processPage.elementUrls["_VVHqUbVXEfG8usiShKwhWw"] = ["Gestion de Defectos","GestionDefectosPlugin/deliveryprocesses/gestion_defectos_7BC3CA5.html"];
+contentPage.processPage.elementUrls["_wf710LVXEfG8usiShKwhWw"] = ["Reportar defecto","GestionDefectosPlugin/deliveryprocesses/reportar_defecto_4823414.html"];
+contentPage.processPage.elementUrls["_8Th_urVXEfG8usiShKwhWw"] = ["Cerrar defecto","GestionDefectosPlugin/deliveryprocesses/cerrar_defecto_45F8833F.html"];
+contentPage.processPage.elementUrls["_q7-TcrVWEfG8usiShKwhWw"] = ["Análisis de causa raíz","GestionDefectosPlugin/capabilitypatterns/analisis_de_causa_raiz_67AF556A.html"];
+contentPage.processPage.elementUrls["_lawGkbVWEfG8usiShKwhWw"] = ["resolucion","GestionDefectosPlugin/capabilitypatterns/resolucion_753D290A.html"];
